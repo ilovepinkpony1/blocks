5505705e8db6281b1d46e6e9a04f74443fad4e0a
Vitalii <ilovepinkpony1@gmail.com>
2019-05-03 21:36:13 +0300

    s

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 'use strict';
 
-setInterval(() => {
-  const main = document.getElementsByClassName('block');
-  const mainBlock = document.querySelector('.main_conteiner');
-  const ifItsLast = [];
+// setInterval(() => {
+//   const main = document.getElementsByClassName('block');
+//   const mainBlock = document.querySelector('.main_conteiner');
+//   const ifItsLast = [];
     
-  for (let j = 0; j < main.length; j++) {
-    if (main[j].offsetLeft > main[main.length - 1].offsetLeft) {
-      ifItsLast.push(main[j].offsetLeft);
+//   for (let j = 0; j < main.length; j++) {
+//     if (main[j].offsetLeft > main[main.length - 1].offsetLeft) {
+//       ifItsLast.push(main[j].offsetLeft);
+//     }
+//   }
+    
+//   if(ifItsLast.length >= 1) {
+//     mainBlock.classList.remove('center_content');
+//     mainBlock.classList.add("left_content");
+//   }	else {
+//     mainBlock.classList.remove('left_content');
+//     mainBlock.classList.add("center_content");
+//   }
+// }, 20);
+
+const main = document.getElementsByClassName('block');
+const mainBlock = document.querySelector('.main_conteiner');
+window.onresize = function(event) {
+    const ifItsLast = [];
+      
+    for (let j = 0; j < main.length; j++) {
+      if (main[j].offsetLeft > main[main.length - 1].offsetLeft) {
+        ifItsLast.push(main[j].offsetLeft);
+      }
+    }  
+    if(ifItsLast.length >= 1) {
+      mainBlock.classList.remove('center_content');
+      mainBlock.classList.add("left_content");
+    }	else {
+      mainBlock.classList.remove('left_content');
+      mainBlock.classList.add("center_content");
     }
-  }
-    
-  if(ifItsLast.length >= 1) {
-    mainBlock.classList.remove('center_content');
-    mainBlock.classList.add("left_content");
-  }	else {
-    mainBlock.classList.remove('left_content');
-    mainBlock.classList.add("center_content");
-  }
-}, 20);
+    console.log(event); 
+};
 
+const button = document.getElementById('but');
+let div = document.createElement("div");
+div.className ='block';
 
+button.onclick = () => {
+  let div = document.createElement("div");
+  div.className ='block';
+  mainBlock.appendChild(div);
+}
 
 
